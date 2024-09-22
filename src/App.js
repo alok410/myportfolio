e8a94c88931009cf36272d3103397a5e3a985aa7
Alok Patel <alokpatel41001@gmail.com>
2024-09-22 17:02:51 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
@@ -8,7 +6,6 @@ import Projects from "./pages/projects";
 import Contact from "./pages/contact";
 import Notfound from "./pages/404";
 
-import { TRACKING_ID } from "./data/tracking";
 import "./App.css";
 
 function App() {
